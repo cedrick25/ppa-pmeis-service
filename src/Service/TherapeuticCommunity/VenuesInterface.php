370c0ef2c13d07defb0de07ede2bc22333a6220b
jericklasat <ghostephemeral@gmail.com>
2021-10-24 21:06:55 +0800

    Updated VenuesInterface

--- src/Service/TherapeuticCommunity/VenuesInterface.php
+++ src/Service/TherapeuticCommunity/VenuesInterface.php
@@ -5,4 +5,8 @@ namespace App\Service\TherapeuticCommunity;
 interface VenuesInterface
 {
     public function create(string $name):array;
+
+    public function getAll(): array;
+
+    public function deleteById(int $id): array;
 }
