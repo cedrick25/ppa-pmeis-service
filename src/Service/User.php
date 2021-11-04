cac328d6ad2ed67e6df4a2f3e001c61b0f580b9e
jericklasat <ghostephemeral@gmail.com>
2021-11-04 19:47:28 +0800

    Removed region_id

--- src/Service/User.php
+++ src/Service/User.php
@@ -40,7 +40,6 @@ class User implements UserInterface
                 $userAccount["user_account_id"] = (int) $userAccount["user_account_id"];
                 $userAccount["user_detail_id"] = (int) $userAccount["user_detail_id"];
                 $userAccount["field_office_id"] = (int) $userAccount["status"];
-                $userAccount["region_id"] = (int) $userAccount["region_id"];
                 $userAccount["status"] = (int) $userAccount["status"];
                 $userAccount["is_pwd"] = (bool) $userAccount["is_pwd"];
                 $userAccount["is_senior_citizen"] = (bool) $userAccount["is_senior_citizen"];
