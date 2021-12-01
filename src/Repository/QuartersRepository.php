533a9cc367636aa8c413b079d4330f22c634be12
jericklasat <ghostephemeral@gmail.com>
2021-12-02 01:30:45 +0800

    Used session cache tag

--- src/Repository/QuartersRepository.php
+++ src/Repository/QuartersRepository.php
@@ -27,7 +27,7 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 class QuartersRepository extends ServiceEntityRepository
 {
     protected const CACHE_TAG = "quarters";
-    protected const SESSION_ACTIVITY_CACHE_TAG = "session_activities";
+    protected const SESSION_CACHE_TAG = "sessions";
 
     public function __construct(
         ManagerRegistry $registry,
@@ -221,7 +221,7 @@ class QuartersRepository extends ServiceEntityRepository
     {
         $params = [
             'cacheKey' => $this->cacheHelper->getQuartersTCA1Part1Key($id),
-            'cacheTag' => self::SESSION_ACTIVITY_CACHE_TAG
+            'cacheTag' => self::SESSION_CACHE_TAG
         ];
 
         return $this->helper->createCachedResponseCustomQuery($params, function() use ($id) {
