9ece188d88567efce6bc35cb9bd23c70a2419acf
jericklasat <ghostephemeral@gmail.com>
2022-01-10 23:51:45 +0800

    Added default client types

---
+++ migrations/Version20220110150622.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220110150622 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql("INSERT INTO client_types (code, description) VALUES "
+            . "('PS', 'Parolees'),"
+            . "('PR', 'Probationers'),"
+            . "('PD', 'Pardonees'),"
+            . "('JICL', 'JICLs'),"
+            . "('FTMDC', 'FTMDOs'),"
+            . "('Pet', 'Petitioners'),"
+            . "('Term', 'Terminated')"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("TRUNCATE TABLE client_types");
+    }
+}
