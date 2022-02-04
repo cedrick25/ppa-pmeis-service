f9700c6df16870021eb889a3cf8da52d2cce02ea
jericklasat <ghostephemeral@gmail.com>
2022-02-05 03:55:10 +0800

    Finished VPA2

--- src/Report/Table/VPAIC2.php
+++ src/Report/Table/VPAIC2.php
@@ -8,6 +8,7 @@ use App\Common\AppDateHelper;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Writer\Exception;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
@@ -57,6 +58,11 @@ class VPAIC2 implements Form
             'B' . $this->lastFilledOutCellY,
             'For purposes of IQPR only:   Inactive VPAs are those with valid appointments  but not rendering service  during the quarter.');
 
+        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('K')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('S')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('T')->setVisible(false);
+
         return $spreadsheet;
     }
 
@@ -66,31 +72,145 @@ class VPAIC2 implements Form
     public function body(): Spreadsheet
     {
         $spreadsheet = $this->header();
+        $total = [
+            'APPOINTED' => ['F' => 0, 'M' => 0],
+            'REAPPOINTED' => ['F' => 0, 'M' => 0],
+            'INACTIVE' => ['F' => 0, 'M' => 0],
+            'DROPPED' => ['F' => 0, 'M' => 0],
+        ];
 
-        $rowNumber = 1;
-//        foreach ($this->data['rows'] as $row) {
-//            $this->lastFilledOutCellY++;
-//            $middleInitial = $row['middleName'] != null ? substr($row['middleName'], 0, 1) . '.' : '';
-//            $nameOfRecruit = $row['lastName'] . ', ' . $row['firstName'] . ' ' . $middleInitial;
-//            $dateOfBirth = $this->appDateHelper->convertStringToImmutableDate($row['dateOfBirth']);
-//            $dateRecruited = $this->appDateHelper->convertStringToImmutableDate($row['dateRecruited']);
-//
-//            $spreadsheet->getActiveSheet()->setCellValue("A" . $this->lastFilledOutCellY, $rowNumber);
-//            $spreadsheet->getActiveSheet()->setCellValue("B" . $this->lastFilledOutCellY, $nameOfRecruit);
-//            if ($row['gender'] === 'F') {
-//                $spreadsheet->getActiveSheet()->setCellValue('C' . $this->lastFilledOutCellY, '∕');
-//            } else {
-//                $spreadsheet->getActiveSheet()->setCellValue('D' . $this->lastFilledOutCellY, '∕');
-//            }
-//            $spreadsheet->getActiveSheet()->setCellValue("E" . $this->lastFilledOutCellY, $dateOfBirth->format('d-M-y'));
-//            $spreadsheet->getActiveSheet()->setCellValue("F" . $this->lastFilledOutCellY, $dateRecruited->format('d-M-y'));
-//            $spreadsheet->getActiveSheet()->setCellValue("G" . $this->lastFilledOutCellY, $row['recruitingOfficer']);
-//            $spreadsheet->getActiveSheet()->getStyle("A" . $this->lastFilledOutCellY . ":G" . $this->lastFilledOutCellY)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+        foreach ($this->data['rows']['APPOINTED'] as $index=>$row) {
+            $rowNumber = $this->lastFilledOutCellY + $index + 1;
+            $volunteer = $row['volunteer'];
 
-            $rowNumber++;
-//        }
+            $middleInitial = $volunteer['middleName'] != null ? substr($volunteer['middleName'], 0, 1) . '.' : '';
+            $fullName = $volunteer['lastName'] . ', ' . $volunteer['firstName'] . ' ' . $middleInitial;
+            $date = $this->appDateHelper->convertStringToImmutableDate($row['date']);
 
-        $spreadsheet->getActiveSheet()->getStyle('A7:G' . $this->lastFilledOutCellY)->getAlignment()->setHorizontal('center');
+            $spreadsheet->getActiveSheet()->setCellValue("A" . $rowNumber, $fullName);
+            $spreadsheet->getActiveSheet()->setCellValue("D" . $rowNumber, $date->format('d-M-y'));
+            if ($volunteer['gender'] === 'F') {
+                $spreadsheet->getActiveSheet()->setCellValue('E' . $rowNumber, '∕');
+                $total['APPOINTED']['F']++;
+            } else {
+                $spreadsheet->getActiveSheet()->setCellValue('F' . $rowNumber, '∕');
+                $total['APPOINTED']['M']++;
+            }
+            if ($volunteer['isPwd']) {
+                $spreadsheet->getActiveSheet()->setCellValue('G' . $rowNumber, '∕');
+            }
+            if ($volunteer['isSeniorCitizen']) {
+                $spreadsheet->getActiveSheet()->setCellValue('H' . $rowNumber, '∕');
+            }
+
+            $spreadsheet->getActiveSheet()->mergeCells('A' . $rowNumber . ':B' . $rowNumber);
+        }
+
+        foreach ($this->data['rows']['REAPPOINTED'] as $index=>$row) {
+            $rowNumber = $this->lastFilledOutCellY + $index + 1;
+            $volunteer = $row['volunteer'];
+
+            $middleInitial = $volunteer['middleName'] != null ? substr($volunteer['middleName'], 0, 1) . '.' : '';
+            $fullName = $volunteer['lastName'] . ', ' . $volunteer['firstName'] . ' ' . $middleInitial;
+            $date = $this->appDateHelper->convertStringToImmutableDate($row['date']);
+
+            $spreadsheet->getActiveSheet()->setCellValue("I" . $rowNumber, $fullName);
+            $spreadsheet->getActiveSheet()->setCellValue("L" . $rowNumber, $date->format('d-M-y'));
+            if ($volunteer['gender'] === 'F') {
+                $spreadsheet->getActiveSheet()->setCellValue('M' . $rowNumber, '∕');
+                $total['REAPPOINTED']['F']++;
+            } else {
+                $spreadsheet->getActiveSheet()->setCellValue('N' . $rowNumber, '∕');
+                $total['REAPPOINTED']['F']++;
+            }
+            if ($volunteer['isPwd']) {
+                $spreadsheet->getActiveSheet()->setCellValue('O' . $rowNumber, '∕');
+            }
+            if ($volunteer['isSeniorCitizen']) {
+                $spreadsheet->getActiveSheet()->setCellValue('P' . $rowNumber, '∕');
+            }
+
+            $spreadsheet->getActiveSheet()->mergeCells('I' . $rowNumber . ':J' . $rowNumber);
+        }
+
+        foreach ($this->data['rows']['INACTIVE'] as $index=>$row) {
+            $rowNumber = $this->lastFilledOutCellY + $index + 1;
+
+            $middleInitial = $row['middleName'] != null ? substr($row['middleName'], 0, 1) . '.' : '';
+            $fullName = $row['lastName'] . ', ' . $row['firstName'] . ' ' . $middleInitial;
+
+            $spreadsheet->getActiveSheet()->setCellValue("Q" . $rowNumber, $fullName);
+            if ($row['gender'] === 'F') {
+                $spreadsheet->getActiveSheet()->setCellValue('U' . $rowNumber, '∕');
+                $total['INACTIVE']['F']++;
+            } else {
+                $spreadsheet->getActiveSheet()->setCellValue('V' . $rowNumber, '∕');
+                $total['INACTIVE']['F']++;
+            }
+
+            $spreadsheet->getActiveSheet()->setCellValue('W' . $rowNumber, 'Non-reporting');
+            $spreadsheet->getActiveSheet()->getStyle('W' . $rowNumber)->getFont()->setSize(8);
+
+            $spreadsheet->getActiveSheet()->mergeCells('Q' . $rowNumber . ':R' . $rowNumber);
+        }
+
+        foreach ($this->data['rows']['DROPPED'] as $index=>$row) {
+            $rowNumber = $this->lastFilledOutCellY + $index + 1;
+            $volunteer = $row['volunteer'];
+
+            $middleInitial = $volunteer['middleName'] != null ? substr($volunteer['middleName'], 0, 1) . '.' : '';
+            $fullName = $volunteer['lastName'] . ', ' . $volunteer['firstName'] . ' ' . $middleInitial;
+            $date = $this->appDateHelper->convertStringToImmutableDate($row['date']);
+
+            $spreadsheet->getActiveSheet()->setCellValue("X" . $rowNumber, $fullName);
+            $spreadsheet->getActiveSheet()->setCellValue("Y" . $rowNumber, $date->format('d-M-y'));
+            if ($volunteer['gender'] === 'F') {
+                $spreadsheet->getActiveSheet()->setCellValue('Z' . $rowNumber, '∕');
+                $total['DROPPED']['F']++;
+            } else {
+                $spreadsheet->getActiveSheet()->setCellValue('AA' . $rowNumber, '∕');
+                $total['DROPPED']['F']++;
+            }
+            $spreadsheet->getActiveSheet()->setCellValue('AB' . $rowNumber, $row['reason']);
+            $spreadsheet->getActiveSheet()->setCellValue('AC' . $rowNumber, $row['droppingRenewal']);
+        }
+
+        $lastFilledY = max(
+            count($this->data['rows']['APPOINTED']),
+            count($this->data['rows']['REAPPOINTED']),
+            count($this->data['rows']['INACTIVE']),
+            count($this->data['rows']['DROPPED'])
+        );
+        $this->lastFilledOutCellY += $lastFilledY;
+
+        $this->lastFilledOutCellY++;
+        $spreadsheet->getActiveSheet()->setCellValue('A' . $this->lastFilledOutCellY, 'TOTAL');
+        $spreadsheet->getActiveSheet()->mergeCells('A' . $this->lastFilledOutCellY . ':D' . $this->lastFilledOutCellY);
+        $spreadsheet->getActiveSheet()->setCellValue('E' . $this->lastFilledOutCellY, $total['APPOINTED']['F']);
+        $spreadsheet->getActiveSheet()->setCellValue('F' . $this->lastFilledOutCellY, $total['APPOINTED']['M']);
+        $spreadsheet->getActiveSheet()->setCellValue('I' . $this->lastFilledOutCellY, 'TOTAL');
+        $spreadsheet->getActiveSheet()->mergeCells('I' . $this->lastFilledOutCellY . ':L' . $this->lastFilledOutCellY);
+        $spreadsheet->getActiveSheet()->setCellValue('M' . $this->lastFilledOutCellY, $total['REAPPOINTED']['F']);
+        $spreadsheet->getActiveSheet()->setCellValue('N' . $this->lastFilledOutCellY, $total['REAPPOINTED']['M']);
+        $spreadsheet->getActiveSheet()->setCellValue('Q' . $this->lastFilledOutCellY, 'TOTAL');
+        $spreadsheet->getActiveSheet()->mergeCells('Q' . $this->lastFilledOutCellY . ':T' . $this->lastFilledOutCellY);
+        $spreadsheet->getActiveSheet()->setCellValue('U' . $this->lastFilledOutCellY, $total['INACTIVE']['F']);
+        $spreadsheet->getActiveSheet()->setCellValue('V' . $this->lastFilledOutCellY, $total['INACTIVE']['M']);
+        $spreadsheet->getActiveSheet()->setCellValue('X' . $this->lastFilledOutCellY, 'TOTAL');
+        $spreadsheet->getActiveSheet()->mergeCells('X' . $this->lastFilledOutCellY . ':Y' . $this->lastFilledOutCellY);
+        $spreadsheet->getActiveSheet()->setCellValue('Z' . $this->lastFilledOutCellY, $total['DROPPED']['F']);
+        $spreadsheet->getActiveSheet()->setCellValue('AA' . $this->lastFilledOutCellY, $total['DROPPED']['M']);
+        $spreadsheet->getActiveSheet()
+            ->getStyle("W" . $this->lastFilledOutCellY)
+            ->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('80808080');
+        $spreadsheet->getActiveSheet()
+            ->getStyle("AB" . $this->lastFilledOutCellY . ":AC" . $this->lastFilledOutCellY)
+            ->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('80808080');
+
+
+        $spreadsheet->getActiveSheet()->getStyle('A8:AC' . $this->lastFilledOutCellY)->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+        $spreadsheet->getActiveSheet()->getStyle('A8:AC' . $this->lastFilledOutCellY)->getAlignment()->setHorizontal('center');
+        $spreadsheet->getActiveSheet()->getStyle('A8:AC' . $this->lastFilledOutCellY)->getAlignment()->setVertical('center');
 
         return $spreadsheet;
     }
@@ -121,8 +241,8 @@ class VPAIC2 implements Form
         $verticalAlignedCoordinates = ['A3:AC7' => 'center'];
         $horizontalAlignedCoordinates = ['A3:AC7' => 'center'];
         $adjustedColumnWidthCoordinates = [
-            'A' => 20, 'B' => 20, 'D' => 7, 'E' => 4, 'F' => 4, 'G' => 5, 'H' => 4, 'I' => 20, 'J' => 20, 'L' => 7, 'M' => 4,
-            'N' => 4, 'O' => 6, 'P' => 4, 'Q' => 20, 'R' => 20, 'U' => 4, 'V' => 4, 'W' => 15, 'X' => 17, 'Y' => 7, 'Z' => 4,
+            'A' => 20, 'B' => 20, 'D' => 12, 'E' => 4, 'F' => 4, 'G' => 5, 'H' => 4, 'I' => 20, 'J' => 20, 'L' => 12, 'M' => 4,
+            'N' => 4, 'O' => 6, 'P' => 4, 'Q' => 20, 'R' => 20, 'U' => 4, 'V' => 4, 'W' => 15, 'X' => 17, 'Y' => 12, 'Z' => 4,
             'AA' => 4, 'AB' => 20, 'AC' => 30
         ];
         $outlineBorderThinCoordinates = [
