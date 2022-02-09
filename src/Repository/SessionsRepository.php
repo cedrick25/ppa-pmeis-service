70b304d1977265b3ce80515a4659d54ab156e1b8
jericklasat <ghostephemeral@gmail.com>
2022-02-09 21:24:09 +0800

    Fixed TC2-6

--- src/Repository/SessionsRepository.php
+++ src/Repository/SessionsRepository.php
@@ -463,20 +463,30 @@ class SessionsRepository extends ServiceEntityRepository
         $minDate = $minMaxDate['min'];
         $maxDate = $minMaxDate['max'];
 
-        $sql = "SELECT c.last_name, c.first_name, c.middle_name, c.suffix, c.gender, c.is_pwd, c.is_senior_citizen, c.date_of_birth,
+        $sql = "SELECT c.last_name, c.first_name, c.middle_name, c.suffix, c.gender, c.is_pwd, c.is_senior_citizen, c.date_of_birth, sr.name as remarks,
                 c.offense_category, ct.code as client_type ,c.supervision_start, c.supervision_end, p.name as phase, se.date, YEAR(se.date) as quarter_year
                 FROM sessions as se 
                 LEFT JOIN client_sessions as cs ON se.session_id = cs.session_id 
                 LEFT JOIN clients as c ON cs.client_id = c.client_id 
                 LEFT JOIN client_types as ct ON c.client_type_id = ct.client_type_id 
                 LEFT JOIN phases as p ON se.phase_id = p.phase_id 
+                LEFT JOIN session_remarks as sr ON se.remarks_id = sr.session_remark_id 
                 WHERE cs.role = '$role' AND c.client_id IS NOT NULL AND se.date BETWEEN CAST('$minDate' AS DATE) AND CAST('$maxDate' AS DATE) 
                 ORDER BY ct.code";
 
         $stmt = $conn->prepare($sql);
         $query = $stmt->executeQuery();
+        $rows = $query->fetchAllAssociative();
 
-        return $query->fetchAllAssociative();
+        $sessions = [];
+        foreach ($rows as $row) {
+            $date = explode('-', $row['date']);
+            $row['quarter'] = $this->appDateHelper->getQuarterByMonth(intval($date[1]));
+
+            $sessions[] = $row;
+        }
+
+        return $sessions;
     }
 
     /**
