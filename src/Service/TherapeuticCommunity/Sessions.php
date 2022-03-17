9a12e9aba17d59937d6730c2e9aa2e9fcb35e1e8
jericklasat <ghostephemeral@gmail.com>
2022-03-18 02:52:33 +0800

    Added catch message

--- src/Service/TherapeuticCommunity/Sessions.php
+++ src/Service/TherapeuticCommunity/Sessions.php
@@ -282,7 +282,7 @@ class Sessions implements SessionsInterface
                 'percentageOfClientsAttendingTC' => $percentageOfClientsAttendingTC
             ]);
         } catch (Exception $e) {
-            return $this->appFormatter->formatResponse(ResponseEnum::FETCHING_FAILED, null, ['app' => $e->getMessage()]);
+            return $this->appFormatter->formatResponse(ResponseEnum::FETCHING_FAILED, null, ['app' => $e->getMessage(), $e->getLine(), $e->getTrace()]);
         } catch (\Doctrine\DBAL\Driver\Exception $e) {
             return $this->appFormatter->formatResponse(ResponseEnum::FETCHING_FAILED, null, ['orm' => $e->getMessage()]);
         }
