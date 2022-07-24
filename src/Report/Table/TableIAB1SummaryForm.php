139827786d8486dc020a626716ef7ff38ca09035
Jerick Son Icamina <ghostephemeral@gmail.com>
2022-07-25 01:59:48 +0800

    Added process in TableIAB1SummaryForm

--- src/Report/Table/TableIAB1SummaryForm.php
+++ src/Report/Table/TableIAB1SummaryForm.php
@@ -16,6 +16,10 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 class TableIAB1SummaryForm implements Form
 {
     private const TABLE_NAME = "TableIAB1SummaryForm";
+    private const PRE_ENCOUNTER_ACT = 'Pre-Encounter Activities';
+    private const COMMUNITY_WORK_SERVICES = 'Community Work Services';
+    private const RESTORED_RELATIONSHIPS = 'Restored Relationships';
+    private const ACTIVE_SUPERVISION = 'Active Supervision';
 
 
     public function __construct(
@@ -58,6 +62,7 @@ class TableIAB1SummaryForm implements Form
         $spreadsheet->getActiveSheet()->getStyle('A7:Y10')->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
         $spreadsheet->getActiveSheet()->getStyle('A13:Y16')->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
         $spreadsheet->getActiveSheet()->getStyle('A18:D24')->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+        $spreadsheet->getActiveSheet()->getStyle('A35:C42')->getBorders()->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
 
         return $spreadsheet;
     }
@@ -68,11 +73,19 @@ class TableIAB1SummaryForm implements Form
         $coordinateX = ['ACTIVE_SUPERVISION' => 10, 'PETITIONER' => 16];
 
         $activitiesCoordinateY = [
-            'Pre-Encounter Activities' => ['Conducted' => 'A', 'F' => 'B', 'M' => 'C', 'PWD' => 'D', 'SC' => 'E'],
+            self::PRE_ENCOUNTER_ACT => ['Conducted' => 'A', 'F' => 'B', 'M' => 'C', 'PWD' => 'D', 'SC' => 'E'],
             'Mediation' => ['Conducted' => 'F', 'F' => 'G', 'M' => 'H', 'PWD' => 'I', 'SC' => 'J'],
             'Conferencing' => ['Conducted' => 'K', 'F' => 'L', 'M' => 'M', 'PWD' => 'N', 'SC' => 'O'],
             'COS' => ['Conducted' => 'P', 'F' => 'Q', 'M' => 'R', 'PWD' => 'S', 'SC' => 'T'],
             'Others' => ['Conducted' => 'U', 'F' => 'V', 'M' => 'W', 'PWD' => 'X', 'SC' => 'Y']
+        ];
+
+        $rjProcess = [
+            self::PRE_ENCOUNTER_ACT => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            'Mediation' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            'Conferencing' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            'COS' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            'Others' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0]
         ];
 
         foreach ($this->data['activities'] as $type=>$activity) {
@@ -81,6 +94,10 @@ class TableIAB1SummaryForm implements Form
                     $coordinate = $activitiesCoordinateY[$process][$label] . $coordinateX[$type];
                     $spreadsheet->getActiveSheet()->setCellValue($coordinate, $value);
                 }
+            }
+
+            if (! empty(key($activity))) {
+                $rjProcess[key($activity)][$type]++;
             }
         }
 
@@ -92,8 +109,8 @@ class TableIAB1SummaryForm implements Form
 
         $outcomes = [
             'Restitution' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
-            'Community Work Services' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
-            'Restored Relationships' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            self::COMMUNITY_WORK_SERVICES => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
+            self::RESTORED_RELATIONSHIPS => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0],
             'Others' => ['ACTIVE_SUPERVISION' => 0, 'PETITIONER' => 0]
 
         ];
@@ -108,12 +125,13 @@ class TableIAB1SummaryForm implements Form
             }
 
             $outcomes[$processConducted['rj_outcome_name']][$rjGroup]++;
+            $rjProcess[$processConducted['rjp_type']][$processConducted['rj_group']]++;
         }
 
         $spreadsheet = $this->plotParticulars($particulars, $spreadsheet);
         $spreadsheet = $this->plotOutcomes($outcomes, $spreadsheet);
 
-        return $spreadsheet;
+        return $this->plotProcess($rjProcess, $spreadsheet);
     }
 
     public function footer(): Spreadsheet
@@ -133,8 +151,8 @@ class TableIAB1SummaryForm implements Form
             'A3' => $this->quarters->getName() . ' QTR, ' . $this->quarters->getYear(),
             'A4' => 'I.B.   RESTORATIVE JUSTICE',
             'A5' => "Table I.B.   Number of RJ Processes Conducted/ Clients' Involvement",
-            'A6' => "TOTAL NUMBER (ACTIVE SUPERVISION)",
-            'A7' => 'Pre-Encounter Activities', 'F7' => 'Mediation', 'K7' => 'Conferencing', 'P7' => 'COS', 'U7' => 'Others',
+            'A6' => "TOTAL NUMBER (". self::ACTIVE_SUPERVISION .")",
+            'A7' => self::PRE_ENCOUNTER_ACT, 'F7' => 'Mediation', 'K7' => 'Conferencing', 'P7' => 'COS', 'U7' => 'Others',
             'A8' => '# of Acts', 'B8' => 'SEX', 'D8' => 'PWD', 'E8' => 'SC',
             'F8' => 'Sessions', 'G8' => 'Sex', 'I8' => 'PWD', 'J8' => 'SC',
             'K8' => 'Sessions', 'L8' => 'Sex', 'N8' => 'PWD', 'O8' => 'SC',
@@ -146,7 +164,7 @@ class TableIAB1SummaryForm implements Form
             'P9' => 'Conducted', 'Q9' => 'F', 'R9' => 'M',
             'U9' => 'Conducted', 'V9' => 'F', 'W9' => 'M',
             'A12' => "TOTAL NUMBER (PETITIONERS)",
-            'A13' => 'Pre-Encounter Activities', 'F13' => 'Mediation', 'K13' => 'Conferencing', 'P13' => 'COS', 'U13' => 'Others',
+            'A13' => self::PRE_ENCOUNTER_ACT, 'F13' => 'Mediation', 'K13' => 'Conferencing', 'P13' => 'COS', 'U13' => 'Others',
             'A14' => '# of Acts', 'B14' => 'SEX', 'D14' => 'PWD', 'E14' => 'SC',
             'F14' => 'Sessions', 'G14' => 'Sex', 'I14' => 'PWD', 'J14' => 'SC',
             'K14' => 'Sessions', 'L14' => 'Sex', 'N14' => 'PWD', 'O14' => 'SC',
@@ -158,24 +176,30 @@ class TableIAB1SummaryForm implements Form
             'P15' => 'Conducted', 'Q15' => 'F', 'R15' => 'M',
             'U15' => 'Conducted', 'V15' => 'F', 'W15' => 'M',
             'A18' => 'Table I.B.1(Columns 1 & 10)',
-            'A19' => 'PARTICULARS (RJ STATUS)', 'B19' => 'Number', 'D19' => 'Total', 'B20' => 'Active Supervision', 'C20' => 'Petitioner',
+            'A19' => 'PARTICULARS (RJ STATUS)', 'B19' => 'Number', 'D19' => 'Total', 'B20' => self::ACTIVE_SUPERVISION, 'C20' => 'Petitioner',
             'A21' => 'Resolved', 'A22' => '(Completed, Agreement reached)', 'A23' => 'Unresolved', 'A24' => '(Shelved/Deffered, On-going)',
             'A26' => 'Table I.B.1 (Columns 1 & 11)',
             'A27' => 'PARTICULARS (RJ OUTCOME)',
             'B27' => 'Number',
-            'D27' => 'Total',
-            'B28' => 'Active Supervision',
-            'C28' => 'Petitioner',
-            'A29' => 'Restitution',
-            'A30' => 'Community Work Services',
-            'A31' => 'Restoration of Relationship',
-            'A32' => 'Others'
+            'D27' => 'Total', 'B28' => self::ACTIVE_SUPERVISION, 'C28' => 'Petitioner', 'A29' => 'Restitution',
+            'A30' => self::COMMUNITY_WORK_SERVICES, 'A31' => 'Restoration of Relationship', 'A32' => 'Others',
+            'A34' => 'Table I.B.1 (Columns 6 & 7)',
+            'A35' => 'RJ PROCESS',
+            'B35' => 'TOTAL NUMBER OF',
+            'B36' => self::ACTIVE_SUPERVISION,
+            'C36' => 'Petitioners',
+            'A37' => self::PRE_ENCOUNTER_ACT,
+            'A38' => 'Mediation',
+            'A39' => 'Conferencing',
+            'A40' => 'Circle of Support',
+            'A41' => 'Others (Indigenous Practices, etc.)',
+            'A42' => 'TOTAL',
         ];
 
         $mergesCoordinates = [
             'A7:E7', 'F7:O7', 'P7:T7', 'U7:Y7', 'A13:E13', 'F13:O13', 'P13:T13', 'U13:Y13',
             'A19:A20', 'B18:C18', 'B21:B22', 'C21:C22', 'B23:B24', 'C23:C24', 'D19:D20', 'D21:D22', 'D23:D24',
-            'A27:A28', 'B27:C27', 'D27:D28'
+            'A27:A28', 'B27:C27', 'D27:D28', 'A35:A36', 'B35:C35'
         ];
 
         $boldCoordinates = [
@@ -183,11 +207,11 @@ class TableIAB1SummaryForm implements Form
         ];
 
         $verticalAlignedCoordinates = [
-            'A1:Y15' => 'center', 'A18:D24' => 'center'
+            'A1:Y15' => 'center', 'A18:D24' => 'center', 'A35:C42' => 'center'
         ];
 
         $horizontalAlignedCoordinates = [
-            'A1:Y15' => 'center', 'A18:D24' => 'center'
+            'A1:Y15' => 'center', 'A18:D24' => 'center', 'A35:C42' => 'center'
         ];
 
         $adjustedColumnWidthCoordinates = [
@@ -290,12 +314,13 @@ class TableIAB1SummaryForm implements Form
     {
         $restitutionActive = $outcomes['Restitution']['ACTIVE_SUPERVISION'];
         $restitutionPetitioner = $outcomes['Restitution']['PETITIONER'];
-        $communityWorkServicesActive = $outcomes['Community Work Services']['ACTIVE_SUPERVISION'];
-        $communityWorkServicesPetitioner = $outcomes['Community Work Services']['PETITIONER'];
-        $restoredRelationshipsActive = $outcomes['Restored Relationships']['ACTIVE_SUPERVISION'];
-        $restoredRelationshipsPetitioner = $outcomes['Restored Relationships']['PETITIONER'];
+        $communityWorkServicesActive = $outcomes[self::COMMUNITY_WORK_SERVICES]['ACTIVE_SUPERVISION'];
+        $communityWorkServicesPetitioner = $outcomes[self::COMMUNITY_WORK_SERVICES]['PETITIONER'];
+        $restoredRelationshipsActive = $outcomes[self::RESTORED_RELATIONSHIPS]['ACTIVE_SUPERVISION'];
+        $restoredRelationshipsPetitioner = $outcomes[self::RESTORED_RELATIONSHIPS]['PETITIONER'];
         $othersActive = $outcomes['Others']['ACTIVE_SUPERVISION'];
         $othersPetitioner = $outcomes['Others']['PETITIONER'];
+
         $spreadsheet->getActiveSheet()->setCellValue('B29', $restitutionActive);
         $spreadsheet->getActiveSheet()->setCellValue('C29', $restitutionPetitioner);
         $spreadsheet->getActiveSheet()->setCellValue('D29', intval($restitutionActive) + intval($restitutionPetitioner));
@@ -308,6 +333,53 @@ class TableIAB1SummaryForm implements Form
         $spreadsheet->getActiveSheet()->setCellValue('B32', $othersActive);
         $spreadsheet->getActiveSheet()->setCellValue('C32', $othersPetitioner);
         $spreadsheet->getActiveSheet()->setCellValue('D32', intval($othersActive) + intval($othersPetitioner));
+
+        return $spreadsheet;
+    }
+
+    /**
+     * @param array<string, array<string, int>> $process
+     */
+    private function plotProcess(array $process, Spreadsheet $spreadsheet): Spreadsheet
+    {
+        $preEncounterActActive = $process[self::PRE_ENCOUNTER_ACT]['ACTIVE_SUPERVISION'];
+        $preEncounterActPetitioner = $process[self::PRE_ENCOUNTER_ACT]['PETITIONER'];
+        $mediationActive = $process['Mediation']['ACTIVE_SUPERVISION'];
+        $mediationPetitioner = $process['Mediation']['PETITIONER'];
+        $conferencingActive = $process['Conferencing']['ACTIVE_SUPERVISION'];
+        $conferencingPetitioner = $process['Conferencing']['PETITIONER'];
+        $cosActive = $process['COS']['ACTIVE_SUPERVISION'];
+        $cosPetitioner = $process['COS']['PETITIONER'];
+        $othersActive = $process['Others']['ACTIVE_SUPERVISION'];
+        $othersPetitioner = $process['Others']['PETITIONER'];
+
+        $totalActive = array_sum([
+            intval($preEncounterActActive),
+            intval($mediationActive),
+            intval($conferencingActive),
+            intval($cosActive),
+            intval($othersActive)
+        ]);
+        $totalPetitioner = array_sum([
+            intval($preEncounterActPetitioner),
+            intval($mediationPetitioner),
+            intval($conferencingPetitioner),
+            intval($cosPetitioner),
+            intval($othersPetitioner),
+        ]);
+
+        $spreadsheet->getActiveSheet()->setCellValue('B37', $preEncounterActActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C37', $preEncounterActPetitioner);
+        $spreadsheet->getActiveSheet()->setCellValue('B38', $mediationActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C39', $mediationPetitioner);
+        $spreadsheet->getActiveSheet()->setCellValue('B39', $conferencingActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C39', $conferencingPetitioner);
+        $spreadsheet->getActiveSheet()->setCellValue('B40', $cosActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C40', $cosPetitioner);
+        $spreadsheet->getActiveSheet()->setCellValue('B41', $othersActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C41', $othersPetitioner);
+        $spreadsheet->getActiveSheet()->setCellValue('B42', $totalActive);
+        $spreadsheet->getActiveSheet()->setCellValue('C42', $totalPetitioner);
 
         return $spreadsheet;
     }
