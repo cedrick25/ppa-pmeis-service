8c5fcc7ccd79e579fbf4a974252162888012d4e2
Jerick Son Icamina <ghostephemeral@gmail.com>
2022-09-08 09:36:13 +0800

    Added get paginated audit trail

---
+++ src/Controller/SystemController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\System\AuditTrail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/system/audit-trail")
+ */
+class SystemController extends AbstractController
+{
+    public function __construct(
+       private AuditTrail $auditTrailService,
+    ) {
+    }
+
+    /**
+     * @Route("/{page}/{pageSize}", methods={"GET"})
+     */
+    public function getPaginated(Request $request): Response
+    {
+        return $this->json($this->auditTrailService->paginated(
+            (int) $request->get("page"),
+            (int) $request->get("pageSize")
+        ));
+    }
+}
