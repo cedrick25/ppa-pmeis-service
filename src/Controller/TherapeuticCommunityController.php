809e888d25128e6b2af1f7286353452a142c012f
Ray Caballero <raycaballero@Rays-MacBook-Pro.local>
2023-08-14 11:35:23 +0800

    Fix bug in creating session activity titles

--- src/Controller/TherapeuticCommunityController.php
+++ src/Controller/TherapeuticCommunityController.php
@@ -305,7 +305,7 @@ class TherapeuticCommunityController extends AbstractController
 
         return $this->json($this->sessionActivitiesService->create(
             $data['name'],
-            $data['name'],
+            // $data['name'],
             $data['phaseId'],
             $data['treatmentCategoryId'],
         ));
