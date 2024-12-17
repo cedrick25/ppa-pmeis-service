0b0b4421bb58cc358708aeef40ca7306e3b45bfe
Jerick Son Icamina <ghostephemeral@gmail.com>
2024-12-17 16:00:48 +0800

    updated end filler

--- src/Service/Volunteerism/Volunteer.php
+++ src/Service/Volunteerism/Volunteer.php
@@ -1214,8 +1214,8 @@ class Volunteer implements VolunteerInterface
         int $index,
     ): string {
         $isEven = $index % 2 == 0;
-        $startFiller = $isEven ? '<td width="1%">&nbsp;</td>' : '';
-        $endFiller = !$isEven ? '<td width="-1%">&nbsp;</td>' : '';
+        $startFiller = $isEven ? '<td width="1%">&nbsp;</td>' : '<td width="3%">&nbsp;</td>';
+        $endFiller = !$isEven ? '<td width="1%">&nbsp;</td>' : '';
         $body = <<<EOD
             {$endFiller}
             <td width="50%">
